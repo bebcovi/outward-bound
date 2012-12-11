60b64523677c6f7c8f7b18af14b16baf8ae0907e
Janko Marohnić <janko.marohnic@gmail.com>
2012-12-11 18:46:02 +0100

    Add real Carousel images

---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,9 @@
+module HomeHelper
+  def carousel_images
+    [
+      {url: "/images/velebit.jpg", title: "Velebitska pustolovina za mlade", subtitle: "Velebit, 2007.", class: "active"},
+      {url: "/images/rafting.jpg", title: "Prezentacijski program rafting", subtitle: "Kupa, 2008."},
+      {url: "/images/boat.jpg",    title: "Ekspedicija instruktora", subtitle: "Split, 2009."},
+    ]
+  end
+end
