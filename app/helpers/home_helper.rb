c7e369f9f058d52bff6405492107ab6306e65ecd
Janko Marohnić <janko.marohnic@gmail.com>
2012-12-13 10:35:14 +0100

    Translate some things

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,9 +1,25 @@
 module HomeHelper
   def carousel_images
     [
-      {url: "/images/velebit.jpg", title: "Velebitska pustolovina za mlade", subtitle: "Velebit, 2007.", class: "active"},
-      {url: "/images/rafting.jpg", title: "Prezentacijski program rafting", subtitle: "Kupa, 2008."},
-      {url: "/images/boat.jpg",    title: "Ekspedicija instruktora", subtitle: "Split, 2009."},
+      {
+        url: "/images/velebit.jpg",
+        title_hr: "Velebitska pustolovina za mlade",
+        title_en: "Velebit adventure for youth",
+        subtitle: "Velebit, 2007.",
+        class: "active"
+      },
+      {
+        url: "/images/rafting.jpg",
+        title_hr: "Prezentacijski program rafting",
+        title_en: "Presentational course rafting",
+        subtitle: "Kupa, 2008."
+      },
+      {
+        url: "/images/boat.jpg",
+        title_hr: "Ekspedicija instruktora",
+        title_en: "Expedition of instructors",
+        subtitle: "Split, 2009."
+      },
     ]
   end
 end
