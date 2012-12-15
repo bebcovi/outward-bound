de1eb9ca195f619d0d676d12256ad3ca3f170330
Janko Marohnić <janko.marohnic@gmail.com>
2012-12-15 14:26:00 +0100

    Internationalize links to partners

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -29,13 +29,15 @@ module HomeHelper
     [
       TranslationHash(
         image: "national-foundation-for-civil-society-development.png",
-        href: "http://zaklada.civilnodrustvo.hr",
+        href_hr: "http://zaklada.civilnodrustvo.hr",
+        href_en: "http://zaklada.civilnodrustvo.hr",
         title_hr: "Nacionalna zaklada<br>za razvoj civilnog društva",
         title_en: "National Foundation<br>for Civil Society Development",
       ),
       TranslationHash(
         image: "vmd.png",
-        href: "http://vmd.hr/en/",
+        href_hr: "http://vmd.hr/hr/",
+        href_en: "http://vmd.hr/en/",
         title_hr: "Putnička agencija VMD",
         title_en: "Travel agency VMD",
       ),
