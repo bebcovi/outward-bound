b92a393f3b3371a6b646e4968ff7f94037c6195d
Janko Marohnić <janko.marohnic@gmail.com>
2012-12-15 11:32:31 +0100

    Make partners code prettier

---
+++ app/helpers/about_helper.rb
@@ -0,0 +1,38 @@
+# encoding: utf-8
+
+module AboutHelper
+  def partners
+    [
+      {
+        image: "outward-bound-international.png",
+        href: "http://www.outwardbound.net/",
+        title_hr: "Outward Bound International",
+        title_en: "Outward Bound International",
+      },
+      {
+        image: "waldorf-school-of-zagreb.png",
+        href: "http://www.waldorfska-skola-zg.skole.hr",
+        title_hr: "Waldorfska škola u Zagrebu",
+        title_en: "Waldorf school of Zagreb",
+      },
+      {
+        image: "udruga-igra.png",
+        href: "http://udrugaigra.hr/",
+        title_hr: "Udruga \"Igra\"",
+        title_en: "Organization \"Igra\"",
+      },
+      {
+        image: "youth-in-action.png",
+        href: "http://ec.europa.eu/youth/index_en.htm",
+        title_hr: "Mladi na djelu",
+        title_en: "Youth in Action",
+      },
+      {
+        image: "international-american-school-of-zagreb.png",
+        href: "http://www.aisz.hr/",
+        title_hr: "Američka škola u Zagrebu",
+        title_en: "International American School of Zagreb",
+      },
+    ]
+  end
+end
