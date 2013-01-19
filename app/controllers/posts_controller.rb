f841c026a8e651b5dde7894c2b7d620f9109d9bc
Janko Marohnić <janko.marohnic@gmail.com>
2013-01-19 01:17:07 +0100

    Cache actions in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.in_language(I18n.locale).order("created_at DESC").paginate(page: params[:page], per_page: 4)
   end
+  caches_action :index
 
   def show
     @post = Post.find(params[:id])
   end
+  caches_action :show
 end
