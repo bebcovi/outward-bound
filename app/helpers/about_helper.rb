636380af7502bd47568573db30126adbbdfb63ac
Matija Marohnić <matija.marohnic@gmail.com>
2013-02-21 19:13:58 +0100

    Finish & optimize those two partners

--- app/helpers/about_helper.rb
+++ app/helpers/about_helper.rb
@@ -52,6 +52,13 @@ module AboutHelper
         title_hr: "Američka škola u Zagrebu",
         title_en: "International American School of Zagreb",
       ),
+      TranslationHash(
+        image: "ured-za-udruge.png",
+        href_hr: "http://www.uzuvrh.hr/defaulthrv.aspx",
+        href_en: "http://www.uzuvrh.hr/defaulteng.aspx",
+        title_hr: "Ured za udruge",
+        title_en: "Office for Cooperation with NGO",
+      ),
     ]
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -29,8 +29,7 @@ module HomeHelper
     [
       TranslationHash(
         image: "national-foundation-for-civil-society-development.png",
-        href_hr: "http://zaklada.civilnodrustvo.hr",
-        href_en: "http://zaklada.civilnodrustvo.hr",
+        href: "http://zaklada.civilnodrustvo.hr",
         title_hr: "Nacionalna zaklada<br>za razvoj civilnog društva",
         title_en: "National Foundation<br>for Civil Society Development",
       ),
@@ -40,6 +39,13 @@ module HomeHelper
         href_en: "http://vmd.hr/en/",
         title_hr: "Putnička agencija VMD",
         title_en: "Travel agency VMD",
+      ),
+      TranslationHash(
+        image: "ured-za-udruge.png",
+        href_hr: "http://www.uzuvrh.hr/defaulthrv.aspx",
+        href_en: "http://www.uzuvrh.hr/defaulteng.aspx",
+        title_hr: "Ured za udruge",
+        title_en: "Office for<br>Cooperation with NGO",
       ),
     ]
   end
