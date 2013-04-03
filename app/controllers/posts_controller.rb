ee32736d18090a3302765b04b1b107a8cd907fca
Janko Marohnić <janko.marohnic@gmail.com>
2013-04-03 19:59:59 +0200

    Remove caching where there is pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.available_in(current_language).descending.paginate(page: params[:page], per_page: 4)
   end
-  caches_action :index
+  # caches_action :index
 
   def show
     @post = Post.find(params[:id])
