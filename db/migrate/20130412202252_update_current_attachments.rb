196458febd44a9ac0df90607935228d2a0eef238
Janko Marohnić <janko.marohnic@gmail.com>
2013-04-13 00:12:25 +0200

    Fix a migration

--- db/migrate/20130412202252_update_current_attachments.rb
+++ db/migrate/20130412202252_update_current_attachments.rb
@@ -3,8 +3,11 @@ class UpdateCurrentAttachments < ActiveRecord::Migration
   end
 
   def up
-    Attachment.find_by_name_hr("Prijavnica").update_attributes(url_hr: "https://dl.dropboxusercontent.com/u/16783504/Application_form_hr.doc")
-    Attachment.find_by_name_hr("Infoletter").update_attributes(url_hr: "https://dl.dropboxusercontent.com/u/16783504/Uskrs-Infoletter.pdf")
+    begin
+      Attachment.find_by_name_hr!("Prijavnica").update_attributes(url_hr: "https://dl.dropboxusercontent.com/u/16783504/Application_form_hr.doc")
+      Attachment.find_by_name_hr!("Infoletter").update_attributes(url_hr: "https://dl.dropboxusercontent.com/u/16783504/Uskrs-Infoletter.pdf")
+    rescue ActiveRecord::RecordNotFound
+    end
   end
 
   def down
