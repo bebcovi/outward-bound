1e5f7431c5591a4bd6c98c96a49601cf253aead9
Janko Marohnić <janko.marohnic@gmail.com>
2013-05-30 14:02:39 +0200

    Display course price always in euros

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,14 +1,6 @@
 # encoding: utf-8
 
 module CoursesHelper
-  def money(value)
-    if croatian?
-      "#{value} Kn"
-    else
-      "#{value} €"
-    end
-  end
-
   def days(value)
     if croatian?
       value == 1 ? "1 dan" : "#{value} dana"
