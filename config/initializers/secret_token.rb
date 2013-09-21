13fda5bc2af19f83edce11caca2bebe246e0d390
Janko Marohnić <janko.marohnic@gmail.com>
2013-09-21 13:40:49 +0200

    Remove secret token from the code

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-OutwardBound::Application.config.secret_key_base = 'ec32e6a537c196deb7574dd3b41b008f7937a869a45b4640d0a85cced4408c8da8ab01b3b546a1a08bbad9a4f80a05017a7b0db243792814850f8e9d0da0716f'
+OutwardBound::Application.config.secret_key_base = ENV["SECRET_TOKEN"]
